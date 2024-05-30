29f5d3b5f72cad64e1b0e5b3f65b8ad371a83579
Aman Verma <9330nama@gmail.com>
2024-05-30 22:59:45 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -34,6 +34,13 @@ const reviews = [
         job: "Data Analyst",
         image: "https://avatars.githubusercontent.com/u/93366359?v=4",
         text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+    },
+    {
+        id: 6,
+        name: "Aman Verma",
+        job: "Fullstack Developer",
+        image: "https://scontent.fccu2-1.fna.fbcdn.net/v/t39.30808-6/336290796_892712891948560_6865072064868040750_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=QreRAGjS88MQ7kNvgEc3OuT&_nc_ht=scontent.fccu2-1.fna&oh=00_AYDBmW-4ZI092mYrm2PKd3m5O-WQ3honfgz1r-VIUWTnSQ&oe=665E95CD",
+        text: "I'm Aman Verma, a second-year B.Tech student specializing in Electronics and Communication Engineering at IEM Kolkata. With a strong foundation in MERN Stack web development, I am passionate about creating dynamic and responsive web applications. I am eager to leverage my technical skills and academic knowledge to contribute effectively to innovative projects.",
     }
 ];
 
